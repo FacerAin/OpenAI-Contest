c7f8c483ed28e12d3d46cb346f942ab47dab4817
FacerAin <syw5141@daum.net>
2019-10-11 20:49:16 +0900

    Fixed ServerSide

--- expo-guider/util/datasend.js
+++ expo-guider/util/datasend.js
@@ -1,4 +1,3 @@
-const rp = require("request-promise");
 
 keywordSend = (keywordData) => {
     let datajson = {
@@ -12,13 +11,17 @@ keywordSend = (keywordData) => {
         body: datajson,
         json: true
     }
-    rp.post(options)
-        .then((body) => {
-            console.log(body);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+    fetch('http://localhost:3000/api/cliConnection',{
+        method: 'POST',
+        body: JSON.stringify(datajson),
+        headers:{
+            'Content-Type': 'application/json'
+          }
+    }).then(res => res.json())
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(error => console.error('Error:', error));
+    
+
 }
 
 module.exports = keywordSend;
