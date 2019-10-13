f723bdcbbaa871d6dc158d7fd9931df3d7d50361
bluejoyq <bluejoyq@naver.com>
2019-10-13 21:19:21 +0900

    colorful~

---
+++ expo-guider/components/Searchbar/Searchbar.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Button,Dimensions,Image,TextInput,TouchableOpacity } from 'react-native';
+import SendToApi from '../../util/datasend'
+import styles from './SearchBarStyles'
+import { connect } from 'react-redux';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {setData} from '../../action'
+class Searchbar extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            search: '',
+            dataset: 'test',
+            fetching: false,
+        }
+        this.props.dispatch(setData(this.state.dataset))
+        console.log(this.props.value)
+    }
+    updateSearch = search => {
+        this.setState({ search });
+        console.log(this.state.search)
+      }
+      
+      sendSearch = async() => {
+        console.log('sendSearch')
+        let resdata = await SendToApi(this.state.search)
+        await this.asyncstate(resdata)
+        await this.props.dispatch(setData(this.state))
+      }
+      sendVoice = () =>{
+        console.log(this.state)
+      }
+
+      render(){
+          return(
+              <>
+              <View style={styles.statusBar}/>
+            <View style={styles.searchContainer}>
+            <View style={styles.searchbar}>
+            <TouchableOpacity style={styles.logo}>
+            <Icon name="arrow-right" size={30} color="#dbe2ef" />
+            </TouchableOpacity>       
+              <TextInput
+              style = {styles.searchText}
+              autoCorrect= {false}
+              placeholder = '  검색'
+              value = {this.state.search}
+              onChangeText={this.updateSearch}
+              onSubmitEditing = {this.sendSearch}
+              />
+            <TouchableOpacity style={styles.searchMic} onPressOut={this.sendVoice}>
+            <Icon name="microphone" size={30} color="#ffffff" />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.searchBtn} onPressOut={this.sendSearch}>
+            <Icon name="search" size={30} color="#ffffff" />
+            </TouchableOpacity>
+            </View>
+            </View>
+              </>
+          )
+      }
+}
+let mapStateToProps = (state) => {
+    return {
+        value : state.processdata.value
+    }
+}
+export default connect(mapStateToProps)(Searchbar)
