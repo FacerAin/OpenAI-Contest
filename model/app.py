cd43a6d8d71cbb9bfdd1d08d20d3ac1c0a629888
FacerAin <syw5141@daum.net>
2019-11-23 02:11:04 +0900

    Fix app.js version control problem

--- model/app.py
+++ model/app.py
@@ -1,8 +1,9 @@
-
 from flask import Flask, redirect, url_for, request
 from gensim.models.doc2vec import Doc2Vec
 from konlpy.tag import Mecab
+import json
 import sys
+
 model = Doc2Vec.load("doc2vec.model")
 
 
@@ -10,19 +11,31 @@ app = Flask(__name__)
 
 
 @app.route('/analyze', methods=['POST'])
-def parsetoken():
-    print("enter")
-    sentence = request.form
-    print(sentence)
+def sendreq():
+    try:
+        data = request.data.decode('utf-8').replace("'", '"')
+        datadic = json.loads(data)
+        sentence1 = datadic["sentence1"]
+        sentence2 = datadic["sentence2"]
+        token1 = parsetoken(sentence1)
+        token2 = parsetoken(sentence2)
+        num_similar = model.n_similarity(token1, token2)
+        return json.dumps({"result": str(num_similar)})
+    except:
+        return json.dumps({"result": -1})
+
+
+def parsetoken(sentence):
     mecab = Mecab()
     lst = []
     tags = mecab.pos(sentence)
     for tag in tags:
         try:
-            model[tag]
+            model["/".join(tag)]
             lst.append("/".join(tag))
         except:
             print("not", tag)
+   # resdata = dict(zip(range(1, len(lst) + 1), lst))
     return lst
 
 
